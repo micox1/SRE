85119b800f756693ac2cb81ad99ccb246b78fdef
TheSlickest1 <micox1@umbc.edu>
2026-04-02 17:07:05 -0400

    Project 4 and 5 done

---
+++ Python/Projects/Python_Class_Projects/Project_4.py
@@ -0,0 +1,128 @@
+'''
+def __init__(self, title, author, year, available= True):
+    Using __init__ is where you would attach attributes to the class 
+@staticmethod def is_valid_year(year):
+    Why is this a static method? And what is this doing?
+        It is static because it isn't about the class or self which is the instance it simply whats to make sure something within the class is true or false
+def __repr__(self)
+    This allows you to print out readable content when you use print on an instance from the class if not there you would basically get where the object is in memory 
+'''
+class Book:
+    def __init__(self, title, author, year, available= True):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.available = available 
+
+    @staticmethod
+    def is_valid_year(year):
+        if year > 0:
+            return True 
+        else:
+            return False
+    
+    def __repr__(self):
+        return f'{self.title}, {self.author}, {self.year}'
+
+'''
+def __init__(self):
+    #Again we are making the blueprint of the class 
+    #Here you want to truly think about what data is going to be going into the Class. A book would need authors, titles, etc. A library would just need a list of books. Think about this for other 
+    instances as well. Cars would be make, model,engine, etc. Make sure that is your thought process when using __init__
+self.books = []
+    #The function of this class is to hold books fromm our Book class
+        def add_book(self, book):
+
+if book.is_valid_year(book.year) == True:
+    self.books.append(book)
+    #So we gave our add_book method a rule that if it has a valid year to append it to the self.books list 
+
+def find_by_author(self, author):
+    results = []
+        for book in self.books:
+            if book.author == author:
+                results.append(book.title)
+        return results
+    #Here we want to be able to find books by a certain author within self.books. So you would have to iterate through self.books and then find books that match that author
+    #book should have these attributes  self.title = title, self.author = author, self.year = year, self.available = available. So if book.author equals the author given it should return the books 
+
+def checkout(self, title):
+    #here we are trying to make sure books can be checked out. Again we need to iterate over the books we have and if the title is within the one given mark it as checked out
+    for book in self.books:
+            if book.title == title:
+                book.available= False 
+    #the for loop would need to iterate over our list of self.books and find the authors that match
+    #remember not to use self.* within the for loop block of code because self here would refer to the Library class and not the book in self.books
+
+
+
+
+'''
+
+class Library:
+
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        if book.is_valid_year(book.year) == True:
+            self.books.append(book)
+    def find_by_author(self, author):
+        results = []
+        for book in self.books:
+            if book.author == author:
+                results.append(book.title)
+        return results
+    
+    def checkout(self, title):
+        for book in self.books:
+            if book.title == title:
+                book.available= False 
+    def return_book(self, title): 
+        for book in self.books:
+            if book.title == title:
+                book.available= True 
+    @classmethod
+    def from_list(cls, book_data):
+        new_data = cls()
+        for books in book_data:
+            title = books["title"]
+            author = books["author"]
+            year = books["year"]
+            b = Book(title,author,year)
+            new_data.add_book(b)
+        return new_data
+
+'''
+Write a `@classmethod` on `Library` called `from_list(cls, book_data)` that takes a list of dictionaries (each with title, author, year) and returns a fully populated `Library` instance.     
+    new_data = cls()
+        This step is creating an empty list meaning, what you currently have inside our __init__ method with self.books would be empty 
+    for books in book_data 
+        Now the objective of this part of the code is to take data in the form of a dicionary and change it to the correct form for the Library
+        So we will iterate through the data that was given using the for loop 
+    title = books["title"], author = books["author"], year = books["year"]
+        Here we are parsing through the data and assigning it correct values 
+    b = Book(title,author,year)
+        Now comes the part where we are converting it into a book for our library 
+        The Book Class now takes the title, the auther, and the year that was just parsed and makes it into a Book Object for our Library
+    new_data.add_book(b)
+        Now if you go to the top of the library I have an add_book method
+            This method checks if the book has a valid year and adds it to self.books or makes it into an instance of library 
+        This step is just adding the new data as an instance 
+'''
+
+    
+
+
+'''
+my_book = Book("1984", "George Orwell", 1949)
+your_book = Book("Animal Farm", "George Orwell", 1945)
+library = Library()
+library.add_book(my_book)
+library.add_book(your_book)
+print(library.books)
+
+library.checkout("1984")
+    
+print(library.find_by_author("George Orwell"))
+'''
+
---
+++ Python/Projects/Python_Class_Projects/Project_5.py
@@ -0,0 +1,80 @@
+import math as math
+
+class Shape:
+    def __init__(self, color):
+        self.color = color
+    def describe(self):
+        print(f'Shape color {self.color}')
+    @staticmethod
+    def is_positive(*values):
+        for v in values:
+            if v <= 0:
+                return False
+        #Point of confusion
+        return True
+
+class Circle(Shape):
+    def __init__(self, color, radius):
+        #Point of confusion: The logic here and raise ValueError
+        if not Shape.is_positive(radius):
+            raise ValueError("Radius must be positive")
+        #Point of confusion: super(). is something i need to review
+        super().__init__(color)
+        self.radius = radius
+    def area(self):
+        a = math.pi * self.radius**2
+        return a
+    def perimeter(self):
+        p = 2 * math.pi * self.radius
+        return p
+    def describe(self):
+        print(f'Circle color: {self.color}')
+    
+
+class Rectangle(Shape):
+    def __init__(self, color, width, height):
+        if not Shape.is_positive(width,height):
+            raise ValueError ("Must be positive integer")
+        super().__init__(color)
+        self.width = width 
+        self.height = height 
+    def area(self):
+        a2 = self.width * self.height 
+        return a2 
+    def perimeter(self):
+        p2 = 2 * (self.height + self.width)
+        return p2
+    
+    @classmethod 
+    def square(cls, side, color):
+        new_square = cls(color, side, side)
+        return new_square
+        
+
+
+
+class Triangle(Shape):
+    def __init__(self, color, a, b, c):
+        if not Shape.is_positive(a,b,c):
+            raise ValueError("Must be positive")
+        super().__init__(color)
+        self.a = a
+        self.b = b
+        self.c = c
+    def area(self):
+        a3 = (self.a + self.b + self.c)/2
+        return a3
+    def perimeter(self):
+        p3 = self.a + self.b + self.c
+        return p3
+
+    
+    
+        
+
+
+
+
+#class Circle:
+#class Rectangle:
+#class Triangle:
